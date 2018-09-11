8e9b238ed69e01da67eccd8adf9750c906c4af90
Youp Wiggers <wigg0003@hz.nl>
2018-09-11 13:36:18 +0200

    added object movement script and kyle, fixed bug in steamvr

--- ShootingSimulator/Assets/Scripts/EnemyTarget.cs
+++ ShootingSimulator/Assets/Scripts/EnemyTarget.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyTarget : MonoBehaviour {
-	public Trandform[] target;
+	public Transform[] target;
 	public float speed;
 
 	private int current;
@@ -14,7 +14,9 @@ public class EnemyTarget : MonoBehaviour {
 	void Update () {
 		if (transform.position != target[current].position)
 		{
-			Vector3 pos =
-		}	
+			Vector3 pos = Vector3.MoveTowards(transform.position,target[current].position, speed * Time.deltaTime);
+			GetComponent<Rigidbody>().MovePosition(pos);
+		}
+		else current = (current + 1) % target.Length; 	
 	}
 }
