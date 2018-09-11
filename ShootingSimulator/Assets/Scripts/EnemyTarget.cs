ea63807a1413c75d1b5a775a57363adb72e9267f
Youp Wiggers <wigg0003@hz.nl>
2018-09-11 10:35:57 +0200

    steamvr and standard assets added plz merge

---
+++ ShootingSimulator/Assets/Scripts/EnemyTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTarget : MonoBehaviour {
+	public Trandform[] target;
+	public float speed;
+
+	private int current;
+
+	
+	
+
+	void Update () {
+		if (transform.position != target[current].position)
+		{
+			Vector3 pos =
+		}	
+	}
+}
